aee6a64262060690a19e7f0fbab76763e4fa124d
RavulaThrinath <thrinathravual@gmail.com>
2022-04-02 23:30:24 +0530

    work

--- src/Components/BlogPosts/BlogPost1.js
+++ src/Components/BlogPosts/BlogPost1.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import client from "../../client";
 import BlockContent from "@sanity/block-content-to-react";
